8ec13fa7451d854bfaf06b1cc38536a120f5d9b2
Yongxiangshi <yongxiangshi@lagou.com>
2016-10-14 23:32:02 +0800

    shiro配置完成

---
+++ src/main/java/com/aaron/realm/MyRealm.java
@@ -0,0 +1,23 @@
+package com.aaron.realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * Created by shiyongxiang on 16/10/14.
+ */
+public class MyRealm extends AuthorizingRealm{
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: shiyongxiang
+  Date: 16/10/14
+  Time: 下午11:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>登录</title>
+</head>
+<body>
+<h1>login page</h1>
+<form id="" action="dologin" method="post">
+    <label>User Name</label>
+    <input tyep="text" name="userName" maxLength="40"/>
+    <label>Password</label>
+    <input type="password" name="password"/>
+    <input type="submit" value="login"/>
+</form>
+</body>
+</html>
